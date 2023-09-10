5649d90cdd91955cd5472a05b128615c89a42efa
Yasmen Ahmed <130899829+yasmen310@users.noreply.github.com>
2023-09-10 15:14:51 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+const redSlider=document.getElementById('redSlider');
+const greenSlider=document.getElementById('greenSlider');
+const blueSlider=document.getElementById('blueSlider');
+
+const redValueSpan=document.getElementById('redValue');
+const greenValueSpan=document.getElementById('greenValue');
+const blueValueSpan=document.getElementById('blueValue');
+
+const colorBox=document.getElementById('color-box');
+const copyButton=document.getElementById('copyButton');
+const inputtypeRGBValue=document.getElementById('inputType');
+
+ redSlider.addEventListener('input',updateColor);
+ greenSlider.addEventListener('input',updateColor);
+ blueSlider.addEventListener('input',updateColor); 
+ copyButton.addEventListener('click',copyRGBValue);
+
+function updateColor(){
+    const redValue=redSlider.value;
+    const greenValue=greenSlider.value;
+    const blueValue=blueSlider.value;
+    // console.log(redValue,greenValue,blueValue);
+    const rgbColor=`rgb(${redValue},${greenValue},${blueValue})`
+    console.log(rgbColor);
+    colorBox.style.backgroundColor=rgbColor;
+    redValueSpan.textContent=redValue;
+    greenValueSpan.textContent=greenValue;
+    blueValueSpan.textContent=blueValue;
+    inputtypeRGBValue.textContent=rgbColor;
+
+}
+updateColor();
+function copyRGBValue(){
+  const redValue=redSlider.value;
+  const greenValue=greenSlider.value;
+  const blueValue=blueSlider.value;
+  const rgbColor=`rgb(${redValue},${greenValue},${blueValue})`
+navigator.clipboard.writeText(rgbColor)
+.then(()=>{
+  alert("RGB Color Value Copied to Clipboard:"+rgbColor);
+
+})
+.catch((error)=>{
+  console.error("failed to copy RGB Value",error);
+
+});
+
+}
